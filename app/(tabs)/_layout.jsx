51e8ba54a267a157159c9d963673739831ec6111
alabi-bamidele <bamidele.alabi@gdc-sense.com>
2025-08-25 16:05:09 +0000

    worked on app/(tabs)/_layout,Colors.js

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -1,9 +1,21 @@
 import { Ionicons } from '@expo/vector-icons'
 import { Tabs } from 'expo-router'
+import { Colors } from '../../assets/Colors'
 
 const TabLayout = () => {
   return (
-    <Tabs screenOptions={{headerShown:false}}>
+    <Tabs 
+        screenOptions={{
+          headerShown:false,
+          tabBarActiveTintColor:Colors.PRIMARY,
+          tabBarInactiveTintColor:Colors.dark.text,
+          tabBarStyle:{
+            backgroundColor:Colors.SECONDARY,
+            //paddingBottom:14,
+            //height:75,
+          },
+          tabBarLabelStyle:{fontSize:12,fontWeight:"bold"}
+        }}>
         <Tabs.Screen name='home' options={{title:"Home",tabBarIcon:({color})=>(
           <Ionicons name='home' size={24} color={color}/>
         )}}/>
