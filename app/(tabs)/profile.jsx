c25ba5302c8a9f39601db388bf29e1136dab44c3
alabi-bamidele <bamidele.alabi@gdc-sense.com>
2025-08-25 14:07:13 +0000

    worked ok layout,index,

--- app/(tabs)/profile.jsx
+++ app/(tabs)/profile.jsx
@@ -1,11 +1,11 @@
 import { Text, View } from 'react-native'
 
-const home = () => {
+const profile = () => {
   return (
     <View className="flex-1 justify-center items-center">
-      <Text className="text-xl font-bold">home</Text>
+      <Text className="text-xl font-bold">Profile</Text>
     </View>
   )
 }
 
-export default home
+export default profile
--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,14 @@
-import { Text, View } from "react-native";
+import { router } from "expo-router";
+import { Text, TouchableOpacity, View } from "react-native";
 
 export default function Index() {
   return (
-    <View className="flex-1 items-center justify-center  bg-[#081b29]">
-      <Text className="text-xl font-bold text-[#00abf0]">Edit app/index.tsx to edit this screen.</Text>
-      <Text className="text-xl font-bold text-[#00abf0]">React expo + NativeWind</Text>
+    <View className="flex-1 items-center justify-center  bg-[#00abf0]">
+      <Text className="text-xl font-bold text-[#081b29]">Edit app/index.tsx to edit this screen.</Text>
+      <Text className="text-xl font-bold text-[#081b29]">React expo + NativeWind</Text>
+      <TouchableOpacity onPress={()=> router.push('/(tabs)/home')} className="border border-[#081b29] rounded-lg px-3 py-3 my-5">
+        <Text className="font-bold">Going to Home</Text>
+      </TouchableOpacity>
     </View>
   );
 }
